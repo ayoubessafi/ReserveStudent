068954e6f3eb1b8d69c1aabfb87c78a313429727
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2021-03-10 00:38:56 +0100

    add class Student and migration DB and update it
    add class Student in model with property and add DbSet in DbContext, after that make migration of database and update it

--- ReserveStudent/Data/ApplicationDbContext.cs
+++ ReserveStudent/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ReserveStudent.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,6 @@ namespace ReserveStudent.Data
             : base(options)
         {
         }
+        public DbSet<Student> Students { get; set; }
     }
 }
